3a8e4d55522e15d08472e632ba7d3785703fa0d7
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-01-16 23:25:45 +0600

    2-What is variable declare a variable see output & 3 Variable type Numeric string boolean added

--- 3-Variable-type-Numeric-String-Boolean.js
+++
@@ -1,5 +0,0 @@
-var bananaPrice = 12;
-console.log(typeof bananaPrice);
-
-var seenAfter = 21;
-console.log(seenAfter);
---
+++ 3. Variable type, Numeric, String, Boolean.js
@@ -0,0 +1,16 @@
+var bananaPrice = 12;
+console.log(typeof bananaPrice);
+
+var seenAfter = 21; // number
+console.log(seenAfter);
+
+var name = "Ahsanul Kabir" // string
+console.log(name);
+console.log(typeof name);
+
+
+var isHot = true;
+var isRich = false;
+
+console.log(isHot);
+console.log(typeof isHot);

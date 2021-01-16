d696cf80bb7983a5ad22795896636b379367ae5f
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-01-17 00:16:12 +0600

    5. explore string case change index split added

---
+++ 5. explore string case change index split.js
@@ -0,0 +1,12 @@
+var promise = "I promise I will BECOME a programmer"
+console.log(promise.toLocaleLowerCase());
+
+console.log(promise.toUpperCase());
+
+console.log(promise.indexOf("will"));
+// if not found the output will be -1
+
+//console.log(promise.split("I"));
+// Split a string into substrings using the specified separator and return them as an array.
+
+console.log(promise.split(" "));

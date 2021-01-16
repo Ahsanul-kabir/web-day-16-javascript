4abd385a53a6626641821b2d350b6e8fc3d19fac
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-01-16 23:39:15 +0600

    2-What is variable declare a variable see output & 3 Variable type Numeric string boolean & 4. variable name naming convention and best practice added

---
+++ 4. variable name naming convention and best practice.js
@@ -0,0 +1,27 @@
+var pocket;
+pocket = 50;
+
+console.log(pocket);
+
+var firstName;
+firstName = 'Nayeem';
+
+console.log(firstName);
+
+var fruit = 'Mango';
+console.log(fruit);
+
+
+var a = 5;
+var b = 6;
+var c = a + b;
+
+console.log(c);
+
+
+var firstName = "Abdul";
+var lastName = "Kader";
+var age = 22;
+
+console.log(firstName, " ", lastName, " is ", age, "years old.");
+console.log(firstName + ' ' + lastName);

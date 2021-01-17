81b3ae4b6816bcb14dc5f9fbcdfbd71a0dc615f8
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-01-18 00:14:10 +0600

    10. Marks conditional problem added

---
+++ 10. Multiple conditions, fulfill both conditions, else if .js
@@ -0,0 +1,17 @@
+var marks = 75;
+
+if (marks <= 100 && marks >= 80) {
+    console.log("A+");
+} else if (marks <= 80 && marks >= 70) {
+    console.log("A");
+} else if (marks <= 80 && marks >= 70) {
+    console.log("A");
+} else if (marks <= 70 && marks >= 60) {
+    console.log("A-");
+} else if (marks <= 60 && marks >= 50) {
+    console.log("B");
+} else if (marks <= 50 && marks >= 40) {
+    console.log("C");
+} else {
+    cconsole.log("Fail")
+}

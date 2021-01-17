d820a70b602cc622cb430b6ac56e747989b94630
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-01-17 23:57:28 +0600

    10. Multiple conditions, fulfill both conditions, else if added

---
+++ 10. Multiple conditions, fulfill both conditions, else if.js
@@ -0,0 +1,13 @@
+var jobPaiso = true;
+var savingAmount = 50000;
+
+// && = and, ||=or
+if (jobPaiso == true && savingAmount > 20000) {
+    console.log("Aso tmr jonno patri khuji");
+}
+elseif(jobPaiso = true) {
+    console.log("accha koy din pore dekbo");
+}
+else {
+    console.log("Tor kopale biya nai");
+}

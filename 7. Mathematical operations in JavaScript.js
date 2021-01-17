582fadd7037cb625183652e013106e32b0b8f432
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-01-17 19:38:24 +0600

    Mathematical operations in JavaScript added

---
+++ 7. Mathematical operations in JavaScript.js
@@ -0,0 +1,24 @@
+var price1 = 35;
+var price2 = 20;
+
+var total = price2 + price1; // addition
+console.log(total);
+
+var total = price2 - price1; // subtraction
+console.log(total);
+
+var total = price2 * price1; // multipication
+console.log(total);
+
+var total = price2 / price1; // division
+total = total.toFixed(3);
+console.log(total);
+
+var total = price2 % price1; // reminder
+console.log(total);
+
+price1++; // increment
+console.log(price1)
+
+price1--; // decrement
+console.log(price1)

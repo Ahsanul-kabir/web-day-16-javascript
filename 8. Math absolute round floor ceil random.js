163c4864662050473c0e5cf21ba8ec8eab682255
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-01-17 22:23:20 +0600

    8. Math absolute round floor ceil random added

---
+++ 8. Math absolute round floor ceil random.js
@@ -0,0 +1,33 @@
+var number = -5; // absulute number er khetre number positive or negetive do not metter
+var absolute_number = Math.abs(number);
+
+console.log(absolute_number);
+
+
+
+var number = 5.500000000;
+var result = Math.round(number); // round number kore pelbe
+// 5.499999 = 5
+// 5.500000000 = 6
+console.log(result);
+
+
+var number = 4.99999999;
+var result = Math.ceil(number); // ceil = up e jbe
+console.log(result);
+
+var number = 6.77;
+var result = Math.floor(number); // ceil = up e jbe
+console.log(result);
+
+
+// random number
+var random_number = Math.random();
+console.log(random_number);
+
+
+// so, handle huge number
+var number = Math.random() * 100;
+console.log(number); // make maining full number
+var result = Math.round(number);
+console.log(result);
